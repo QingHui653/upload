45435dc8ab16230dfd7c44a62977e45b46a2191c
DESKTOP-5TMB1MQ\woshizbh <910944453@qq.com>
2017-12-25 09:42:54 +0800

    fix bug model

---
+++ src/main/java/com/example/demo/spring/service/model/nove/Nove.java
@@ -0,0 +1,13 @@
+package com.example.demo.spring.service.model.nove;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Nove {
+    private String id;
+    private String name;
+    private String noveType;
+    private String titleType;
+}
---
+++ src/main/java/com/example/demo/spring/service/model/nove/Title.java
@@ -0,0 +1,12 @@
+package com.example.demo.spring.service.model.nove;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Title {
+    private String tid;
+    private Integer index;
+    private String content;
+}
