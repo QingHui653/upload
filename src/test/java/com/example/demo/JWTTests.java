e5f394a1ca9d3904320a0c9191339e94d1c2fc8f
qh <qh@ivy.com>
2019-05-26 12:30:20 +0800

    Merge branch 'master' of https://github.com/QingHui653/AnewB

--- src/test/java/com/example/demo/JWTTests.java
+++ src/test/java/com/example/demo/JWTTests.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import com.example.demo.web.tool.JWTUtil;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JWTTests {
 
     @Test
@@ -16,5 +20,12 @@ public class JWTTests {
         String token = JWTUtil.sign("admin","123");
         boolean isLogin = JWTUtil.verify(token, "admin", "123");
         System.out.println(isLogin);
+    }
+
+    @Test
+    public void hashmap(){
+
+        Map map = Collections.synchronizedMap (new HashMap()) ;
+        map.put("1","1");
     }
 }
